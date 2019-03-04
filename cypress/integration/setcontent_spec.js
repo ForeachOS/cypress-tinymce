34a436667853391f0908ad028401c3dbf83db265
Christiaan Van den Poel <cvp@foreach.be>
2019-03-04 12:50:56 +0100

    added `setTinyMceContent` and `getTinyMceContent` commands

--- cypress/integration/setcontent_spec.js
+++ cypress/integration/setcontent_spec.js
@@ -1,8 +1,8 @@
 describe('cypress TinyMce plugin', () => {
-  const blogTitle = 'The title of the blog';
-  const blogContent = 'The contents of the blog';
-
   it('has a method to set the content', ()=> {
+    const blogTitle = 'The title of the blog';
+    const blogContent = 'The contents of the blog';
+
     cy.visit('/');
     cy.contains('Create new blog').click();
     cy.getTinyMce('content').setContent(blogContent);
@@ -12,5 +12,30 @@ describe('cypress TinyMce plugin', () => {
 
     cy.contains(blogTitle);
     cy.contains(blogContent);
+  });
+
+  it('has a single command to set the content', () => {
+    const blogTitle = 'The title of the next blog';
+    const blogContent = 'The contents of the next blog';
+
+    cy.visit('/');
+    cy.contains('Create new blog').click();
+    cy.setTinyMceContent('content', blogContent);
+    cy.get('#title').type(blogTitle);
+    cy.wait(200);
+    cy.contains('Save').click();
+
+    cy.contains(blogTitle);
+    cy.contains(blogContent);
+  });
+
+  it('has a command to get the content', () => {
+    const blogTitle = 'The title of the blog';
+    const blogContent = 'The contents of the blog';
+
+    cy.visit('/');
+    cy.contains('Create new blog').click();
+    cy.setTinyMceContent('content', blogContent);
+    cy.getTinyMceContent('content').should('contain', blogContent);
   });
 });
--- index.js
+++ index.js
@@ -8,3 +8,17 @@ Cypress.Commands.add('setContent', {prevSubject: true}, (subject, content) => {
   subject.setContent(content);
   return subject;
 });
+
+Cypress.Commands.add('setTinyMceContent', (tinyMceId, content) => {
+  cy.window().then((win) => {
+    const editor = win.tinymce.editors[tinyMceId];
+    editor.setContent(content);
+  });
+});
+
+Cypress.Commands.add('getTinyMceContent', (tinyMceId, content) => {
+  cy.window().then((win) => {
+    const editor = win.tinymce.editors[tinyMceId];
+    return editor.getContent();
+  });
+});
